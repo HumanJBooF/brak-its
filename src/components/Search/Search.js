9d699ea9abd00c11723318c702922217dcf676ac
HumanJBooF <humanjboof@gmail.com>
2018-11-19 19:04:33 -0500

    lets see if this works

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import Container from '../Container';
 import CreateButton from '../Create';
-
-const searchStyles = {
-
-    marginTop: "5vh",
-    backgroundColor: "grey",
-    color: "white",
-    borderRadius: "20px",
-    zIndex: "5",
-    border: "1px solid white",
-    fontWeight: "Bold",
-    width: "100%",
-
-}
+import styles from './SearchStyles';
 
 const SearchBar = () => (
     <Container>
         <div className="row">
             <form className="col s6">
                 <div className="row center-align left">
-                    <input id="search" type="search" style={searchStyles} />
+                    <input id="search" type="search" style={styles.searchBar} />
                     <label htmlFor="search" className="black-text">Search Tournaments!</label>
                 </div>
             </form>
