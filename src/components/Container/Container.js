b1dc0485cdc9ec04d332de1f6b249f6416ba6b48
Mason Hester <masonhester@Masons-MacBook-Pro.local>
2018-12-02 17:13:15 -0500

    Fixed final winner issue

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Container = ({ fluid, children }) => (
-<<<<<<< HEAD
-    <div 
-=======
     <div
->>>>>>> 05351669d545d9d93ecb4385701f3fa7e27c7421
         className={`container${fluid ? "-fluid" : ""}`}>
         {children}
     </div>
